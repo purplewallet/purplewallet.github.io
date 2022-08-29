0c8b98c448f0f54237e0fd7d86d8cceb56f891d2
purplewallet <112313837+purplewallet@users.noreply.github.com>
2022-08-29 23:13:37 +0300

    Add files via upload

---
+++ precache-manifest.5caeae389c3d159a42664c45ae63ae14.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33b0f3b4279637b5cf8d",
+    "url": "css/about.89a44b1f.css"
+  },
+  {
+    "revision": "baeb61c8e88bed2077c1",
+    "url": "css/app.c3babd25.css"
+  },
+  {
+    "revision": "937c2a6ad6b5d96fa1cf951c5aa30182",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "e667c533141c506d480fcbd7c3244930",
+    "url": "img/bch.e667c533.png"
+  },
+  {
+    "revision": "780d7934637dea2bfb49e2521301f629",
+    "url": "img/bnb.780d7934.png"
+  },
+  {
+    "revision": "73ecd2bc688eab094737b524dae61324",
+    "url": "img/bsv.73ecd2bc.png"
+  },
+  {
+    "revision": "09128095777e3e235ee1326858e177f0",
+    "url": "img/btc.09128095.png"
+  },
+  {
+    "revision": "d9d6e8130809f8a9d5ea30dc069228f7",
+    "url": "img/cosmos.d9d6e813.png"
+  },
+  {
+    "revision": "5b8d834bc82565189fc8fc2144297895",
+    "url": "img/eth.5b8d834b.png"
+  },
+  {
+    "revision": "eb65c7cea8311c2cc412340f7422a82f",
+    "url": "img/secret.eb65c7ce.png"
+  },
+  {
+    "revision": "0eeabf1e82565fec957c08256f1629c1",
+    "url": "img/trx.0eeabf1e.png"
+  },
+  {
+    "revision": "1d92ef19cc4b21232457f71085356735",
+    "url": "index.html"
+  },
+  {
+    "revision": "33b0f3b4279637b5cf8d",
+    "url": "js/about.81469d69.js"
+  },
+  {
+    "revision": "baeb61c8e88bed2077c1",
+    "url": "js/app.38ee184f.js"
+  },
+  {
+    "revision": "f5c722377da41ce0fa57",
+    "url": "js/chunk-vendors.5206d840.js"
+  },
+  {
+    "revision": "85c74cee0a64a65256b58f71991223f0",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.e90de25dce854abf76998acfbf487eaf.js"
+  "precache-manifest.5caeae389c3d159a42664c45ae63ae14.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "plapinp_wallet"});
