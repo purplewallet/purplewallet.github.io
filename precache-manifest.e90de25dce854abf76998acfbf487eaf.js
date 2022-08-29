94fb18f9223c0d54a22baf6abccd1369d40490de
purplewallet <112313837+purplewallet@users.noreply.github.com>
2022-08-29 22:12:16 +0300

    Add files via upload

---
+++ precache-manifest.e90de25dce854abf76998acfbf487eaf.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a436d0f0a2b9c3c39410",
+    "url": "css/about.05635641.css"
+  },
+  {
+    "revision": "4d476c9146b2a689a516",
+    "url": "css/app.ff01fe75.css"
+  },
+  {
+    "revision": "937c2a6ad6b5d96fa1cf951c5aa30182",
+    "url": "favicon.png"
+  },
+  {
+    "revision": "e667c533141c506d480fcbd7c3244930",
+    "url": "img/bch.e667c533.png"
+  },
+  {
+    "revision": "780d7934637dea2bfb49e2521301f629",
+    "url": "img/bnb.780d7934.png"
+  },
+  {
+    "revision": "73ecd2bc688eab094737b524dae61324",
+    "url": "img/bsv.73ecd2bc.png"
+  },
+  {
+    "revision": "09128095777e3e235ee1326858e177f0",
+    "url": "img/btc.09128095.png"
+  },
+  {
+    "revision": "d9d6e8130809f8a9d5ea30dc069228f7",
+    "url": "img/cosmos.d9d6e813.png"
+  },
+  {
+    "revision": "5b8d834bc82565189fc8fc2144297895",
+    "url": "img/eth.5b8d834b.png"
+  },
+  {
+    "revision": "eb65c7cea8311c2cc412340f7422a82f",
+    "url": "img/secret.eb65c7ce.png"
+  },
+  {
+    "revision": "0eeabf1e82565fec957c08256f1629c1",
+    "url": "img/trx.0eeabf1e.png"
+  },
+  {
+    "revision": "f6021c4b00fa6ac941170eb18aac69bd",
+    "url": "index.html"
+  },
+  {
+    "revision": "a436d0f0a2b9c3c39410",
+    "url": "js/about.81469d69.js"
+  },
+  {
+    "revision": "4d476c9146b2a689a516",
+    "url": "js/app.7d4165e1.js"
+  },
+  {
+    "revision": "f5c722377da41ce0fa57",
+    "url": "js/chunk-vendors.5206d840.js"
+  },
+  {
+    "revision": "85c74cee0a64a65256b58f71991223f0",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
